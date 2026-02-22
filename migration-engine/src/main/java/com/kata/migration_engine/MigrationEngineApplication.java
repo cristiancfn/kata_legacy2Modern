164c4677a18afa097fb4b1024dfa4158ceb41400
Cristian Camilo Flórez Novoa <cristian.florezn@gmail.com>
2026-02-22 10:53:39 -0500

    Se realiza la creacion del proyecto base para backend

---
+++ migration-engine/src/main/java/com/kata/migration_engine/MigrationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.kata.migration_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationEngineApplication.class, args);
+	}
+
+}
---
+++ migration-engine/src/test/java/com/kata/migration_engine/MigrationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.migration_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
