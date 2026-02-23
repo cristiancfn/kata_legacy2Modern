402c93ed185882005ab754f750750d05e0d4ffad
Cristian Camilo Flórez Novoa <cristian.florezn@gmail.com>
2026-02-22 19:57:19 -0500

    Se crea una clase de configuracion para indicarle a Spring como instanciar a los expertos y al motor de migracion, todos como Beans

---
+++ migration-engine/src/main/java/com/kata/migration_engine/infraestructure/config/MigrationConfig.java
@@ -0,0 +1,32 @@
+package com.kata.migration_engine.infraestructure.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.kata.migration_engine.application.expert.CobolParser;
+import com.kata.migration_engine.application.expert.JavaGenerator;
+import com.kata.migration_engine.application.service.MigrationEngine;
+import com.kata.migration_engine.domain.port.LegacyParser;
+import com.kata.migration_engine.domain.port.ModernGenerator;
+
+@Configuration
+public class MigrationConfig {
+
+    @Bean
+    LegacyParser cobolParser() {
+        return new CobolParser();
+    }
+
+    @Bean
+    ModernGenerator javaGenerator() {
+        return new JavaGenerator();
+    }
+
+    @Bean
+    MigrationEngine migrationEngine(List<LegacyParser> parsers, List<ModernGenerator> generators) {
+        return new MigrationEngine(parsers, generators);
+    }
+
+}
