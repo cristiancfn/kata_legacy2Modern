c0497ce488a99cf24603f8e736bf5aec15b9b7f6
Cristian Camilo Flórez Novoa <cristian.florezn@gmail.com>
2026-02-22 20:30:21 -0500

    Se implementa un controlador para manejo de errores global en el Backend

---
+++ migration-engine/src/main/java/com/kata/migration_engine/infraestructure/rest/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.kata.migration_engine.infraestructure.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach( error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
